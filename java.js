6548aaf486c3c8d08166ba23e58b45095d8f80fc
roxannemarcil <roxanne.marcil@gmail.com>
2015-03-10 13:35:08 -0400

    -Devoir #1 --- Étape 1/6

--- java.js
+++ java.js
@@ -1,3 +1,16 @@
 /**
  * Created by roxannemarcil on 15-03-09.
  */
+
+alert ("Bienvenue sur mon site !!");
+
+var ageUtilisateur = prompt("Quel âge as-tu?");
+
+if (ageUtilisateur > 0)
+{
+    console.log ("Vous avez"+" "+ ageUtilisateur +" " + "ans");
+}
+
+else {
+    console.log ("Tu n'as pas entré ton âge !!");
+}
