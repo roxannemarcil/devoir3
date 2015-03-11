6e8649baf939f0dd1320fabd469f2eabe25de191
roxannemarcil <roxanne.marcil@gmail.com>
2015-03-10 21:22:41 -0400

    -Devoir #1 --- Étape 5/6

--- java.js
+++ java.js
@@ -2,18 +2,18 @@
  * Created by roxannemarcil on 15-03-09.
  */
 
-alert ("Bienvenue sur mon site !!");
+//alert ("Bienvenue sur mon site !!");
 
-var ageUtilisateur = prompt("Quel âge as-tu?");
+//var ageUtilisateur = prompt("Quel âge as-tu?");
 
-if (ageUtilisateur > 0)
-{
-    console.log ("Vous avez"+" "+ ageUtilisateur +" " + "ans");
-}
+//if (ageUtilisateur > 0)
+//{
+//    console.log ("Vous avez"+" "+ ageUtilisateur +" " + "ans");
+//}
 
-else {
-    console.log ("Tu n'as pas entré ton âge !!");
-}
+//else {
+//    console.log ("Tu n'as pas entré ton âge !!");
+//}
 
 // ÉTAPE 4 //
 
@@ -23,13 +23,27 @@ chiffre = parseInt(chiffre);
 
 console.log (chiffre)
 
-if (isNaN(chiffre))
-{
-    alert ("Mauvaise entrée !!");
-}
-else {
-    alert ("Merci !!");
-}
+//if (isNaN(chiffre))
+//{
+//    alert ("Mauvaise entrée !!");
+//}
+//else {
+//    alert ("Merci !!");
+//}
 
 // ÉTAPE 5 //
 
+var bonneReponse = (11);
+bonneReponse = parseInt(bonneReponse);
+
+if (chiffre === bonneReponse) {
+    alert ("Vous avez trouvez le nombre !! Bravo !");
+}
+else if (chiffre < bonneReponse) {
+    alert ("Votre chiffre est trop petit !!");
+    prompt("Réesayez !");
+}
+else if (chiffre > bonneReponse) {
+    alert ("Votre chiffre est trop grand !!");
+    prompt("Réesayez !");
+}
